b0d1be5372dc1e6289af7f22b44bce24faab4b8f
Quanfei Zhang <qzhang392@gatech.edu>
2020-01-21 00:59:56 -0500

    Create user and add activity

---
+++ app/src/main/java/edu/gatech/payconsulting/UserMainPageActivity.java
@@ -0,0 +1,14 @@
+package edu.gatech.payconsulting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserMainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_main_page);
+    }
+}
