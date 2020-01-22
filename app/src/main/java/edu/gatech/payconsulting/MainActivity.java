4cf536125fb8f53a305a6b158cfc95effd4a63ed
Quanfei Zhang <qzhang392@gatech.edu>
2020-01-22 09:33:54 -0500

    add ListView for Consultings

--- app/src/main/java/edu/gatech/payconsulting/MainActivity.java
+++ app/src/main/java/edu/gatech/payconsulting/MainActivity.java
@@ -1,19 +1,74 @@
 package edu.gatech.payconsulting;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView=(ListView)findViewById(R.id.listview);
+        final ArrayList<String> arrayList=new ArrayList<>();
+//        // Access a Cloud Firestore instance from your Activity
+//        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//        db.collection("records")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                arrayList.add(document.getData().get("consultingDes").toString());
+////                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//                        }
+//                    }
+//                });
+
+
+
+        arrayList.add("Help me on resume");
+        arrayList.add("Consult for headache");
+        arrayList.add("Need a doctor's help");
+        arrayList.add("How should I design my career path?");
+        arrayList.add("Where to rent a cheap house close to Georgia Tech?");
+
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     public void logout(View view) {
